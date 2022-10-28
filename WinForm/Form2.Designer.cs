b16341814d3f7bf7ad4150ffb3480bbb2c081f47
Артём Межинский <aswert092@gmail.com>
2022-10-28 22:22:18 +0300

    Done

--- WinForm/Form2.Designer.cs
+++ WinForm/Form2.Designer.cs
@@ -54,7 +54,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(220, 135);
+            this.button1.Location = new System.Drawing.Point(229, 111);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
@@ -73,7 +73,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(307, 168);
+            this.ClientSize = new System.Drawing.Size(307, 143);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.lb_DRText);
